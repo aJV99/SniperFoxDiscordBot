eb569d23d359c377e961a68398ed986bb8bcb95c
Your Name <you@example.com>
2022-04-24 18:45:10 +0100

    v3.2.0

--- src/bot.js
+++ src/bot.js
@@ -201,6 +201,12 @@ client.on('message', (message) => {
     if (message.author.id == "159985870458322944") {
         message.reply("https://cdn.discordapp.com/attachments/772192764175581196/967723087376289792/Oh_No.mp4");
     }
+    if (message.author.id == "159985870458322944") {
+        message.reply("https://cdn.discordapp.com/attachments/772192764175581196/967723087376289792/Oh_No.mp4");
+    }
+    if (message.author.id === "766238802917457931") {
+        message.reply("Yes Lord");
+    }
 })
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
