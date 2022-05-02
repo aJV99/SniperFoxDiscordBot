1b7354b62c2aae7406b381886835c7f3a3af830b
Your Name <you@example.com>
2022-05-02 18:02:35 +0100

    v4.0.2

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,3 @@
-import { newServer } from './deploy-commands.js';
 const fs = require('fs');
 require('dotenv').config();
 
@@ -217,7 +216,23 @@ client.on('guildCreate', (guild) => {
             console.log("JSON data is saved.");
         });   
     });
-    let val = newServer();
+    const { SlashCommandBuilder } = require('@discordjs/builders');
+    const { REST } = require('@discordjs/rest');
+    const { Routes } = require('discord-api-types/v9');
+    const { clientId, guildId, token } = require('./config.json');
+
+    const commands = [
+        new SlashCommandBuilder().setName('snipe').setDescription('what can i say except undelete this'),
+        new SlashCommandBuilder().setName('last10').setDescription('feelin cute, might undelete this later'),
+        new SlashCommandBuilder().setName('help').setDescription('help me SniperFox Kenobi, youre my only hope'),
+    ]
+        .map(command => command.toJSON());
+
+    const rest = new REST({ version: '9' }).setToken(token);
+
+    rest.put(Routes.applicationCommands(clientId), { body: commands })
+        .then(() => console.log('Successfully registered application commands.'))
+        .catch(console.error);
 })
 
 client.on('guildDelete', (guild) => {
